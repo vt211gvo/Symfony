6ecc208ca94d375b3ca366194e15e09a96ce10ce
Victoria Gavrishchuk <vt211_gvo@student.ztu.edu.ua>
2025-04-17 12:19:31 +0300

    Installed API platform and added CRUD operations to one entity

--- src/Entity/Bed.php
+++ src/Entity/Bed.php
@@ -2,23 +2,56 @@
 
 namespace App\Entity;
 
-use App\Repository\BedRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JsonSerializable;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: BedRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(
+            normalizationContext: ['groups' => 'get:item:bed']
+        ),
+        new GetCollection(
+            normalizationContext: ['groups' => 'get:collection:bed']
+        ),
+        new Post(
+            normalizationContext: ['groups' => 'get:item:bed'],
+            denormalizationContext: ['groups' => 'post:collection:bed']
+        ),
+        new Patch(
+            normalizationContext: ['groups' => 'get:item:bed'],
+            denormalizationContext: ['groups' => 'patch:item:bed']
+        ),
+        new Delete(),
+    ],
+)]
+#[ORM\Entity]
 class Bed implements JsonSerializable
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get:item:bed', 'get:collection:bed'])]
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'beds')]
     #[Assert\NotNull(message: 'Room cannot be null.')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups([
+        'get:item:bed',
+        'get:collection:bed',
+        'post:collection:bed',
+        'patch:item:bed'
+    ])]
     private ?Room $room = null;
 
     #[ORM\Column(length: 255)]
@@ -27,6 +60,12 @@ class Bed implements JsonSerializable
         max: 255,
         maxMessage: 'Bed number cannot be longer than {{ limit }} characters.'
     )]
+    #[Groups([
+        'get:item:bed',
+        'get:collection:bed',
+        'post:collection:bed',
+        'patch:item:bed'
+    ])]
     private ?string $bedNumber = null;
 
     /**
@@ -36,6 +75,7 @@ class Bed implements JsonSerializable
     #[Assert\All([
         new Assert\Type(type: Booking::class, message: 'Each booking must be a valid Booking object.')
     ])]
+    #[Groups(['get:item:bed'])]
     private Collection $bookings;
 
     public function __construct()
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,15 +9,16 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
     public static $files = array (
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '662a729f963d39afe703c9d9b7ab4a8c' => __DIR__ . '/..' . '/symfony/polyfill-php83/bootstrap.php',
-        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
         '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
         'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
+        '662a729f963d39afe703c9d9b7ab4a8c' => __DIR__ . '/..' . '/symfony/polyfill-php83/bootstrap.php',
+        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         '89efb1254ef2d1c5d80096acd12c4098' => __DIR__ . '/..' . '/twig/twig/src/Resources/core.php',
         'ffecb95d45175fd40f75be8a23b34f90' => __DIR__ . '/..' . '/twig/twig/src/Resources/debug.php',
         'c7baa00073ee9c61edf148c51917cfb4' => __DIR__ . '/..' . '/twig/twig/src/Resources/escaper.php',
         'f844ccf1d25df8663951193c3fc307c8' => __DIR__ . '/..' . '/twig/twig/src/Resources/string_loader.php',
+        '09f6b20656683369174dd6fa83b7e5fb' => __DIR__ . '/..' . '/symfony/polyfill-uuid/bootstrap.php',
         '2203a247e6fda86070a5e4e07aed533a' => __DIR__ . '/..' . '/symfony/clock/Resources/now.php',
         'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
@@ -46,6 +47,7 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
             'Symfony\\UX\\Turbo\\' => 17,
             'Symfony\\UX\\StimulusBundle\\' => 26,
             'Symfony\\Runtime\\Symfony\\Component\\' => 34,
+            'Symfony\\Polyfill\\Uuid\\' => 22,
             'Symfony\\Polyfill\\Php83\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Intl\\Normalizer\\' => 33,
@@ -63,6 +65,7 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
             'Symfony\\Component\\VarExporter\\' => 30,
             'Symfony\\Component\\VarDumper\\' => 28,
             'Symfony\\Component\\Validator\\' => 28,
+            'Symfony\\Component\\Uid\\' => 22,
             'Symfony\\Component\\Translation\\' => 30,
             'Symfony\\Component\\String\\' => 25,
             'Symfony\\Component\\Stopwatch\\' => 28,
@@ -128,6 +131,8 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         ),
         'N' => 
         array (
+            'Nelmio\\CorsBundle\\' => 18,
+            'Negotiation\\' => 12,
             'Namshi\\JOSE\\' => 12,
         ),
         'M' => 
@@ -171,6 +176,19 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         array (
             'App\\Tests\\' => 10,
             'App\\' => 4,
+            'ApiPlatform\\Validator\\' => 22,
+            'ApiPlatform\\Symfony\\' => 20,
+            'ApiPlatform\\State\\' => 18,
+            'ApiPlatform\\Serializer\\' => 23,
+            'ApiPlatform\\OpenApi\\' => 20,
+            'ApiPlatform\\Metadata\\' => 21,
+            'ApiPlatform\\JsonSchema\\' => 23,
+            'ApiPlatform\\JsonLd\\' => 19,
+            'ApiPlatform\\Hydra\\' => 18,
+            'ApiPlatform\\HttpCache\\' => 22,
+            'ApiPlatform\\Documentation\\' => 26,
+            'ApiPlatform\\Doctrine\\Orm\\' => 25,
+            'ApiPlatform\\Doctrine\\Common\\' => 28,
         ),
     );
 
@@ -204,6 +222,10 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         'Symfony\\Runtime\\Symfony\\Component\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/runtime/Internal',
+        ),
+        'Symfony\\Polyfill\\Uuid\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-uuid',
         ),
         'Symfony\\Polyfill\\Php83\\' => 
         array (
@@ -272,6 +294,10 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         'Symfony\\Component\\Validator\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/validator',
+        ),
+        'Symfony\\Component\\Uid\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/uid',
         ),
         'Symfony\\Component\\Translation\\' => 
         array (
@@ -509,6 +535,14 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         array (
             0 => __DIR__ . '/..' . '/phpstan/phpdoc-parser/src',
         ),
+        'Nelmio\\CorsBundle\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nelmio/cors-bundle',
+        ),
+        'Negotiation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/willdurand/negotiation/src/Negotiation',
+        ),
         'Namshi\\JOSE\\' => 
         array (
             0 => __DIR__ . '/..' . '/namshi/jose/src/Namshi/JOSE',
@@ -583,7 +617,8 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         ),
         'Doctrine\\Common\\' => 
         array (
-            0 => __DIR__ . '/..' . '/doctrine/event-manager/src',
+            0 => __DIR__ . '/..' . '/doctrine/common/src',
+            1 => __DIR__ . '/..' . '/doctrine/event-manager/src',
         ),
         'Doctrine\\Bundle\\MigrationsBundle\\' => 
         array (
@@ -608,6 +643,58 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         'App\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
+        ),
+        'ApiPlatform\\Validator\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/validator',
+        ),
+        'ApiPlatform\\Symfony\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/symfony',
+        ),
+        'ApiPlatform\\State\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/state',
+        ),
+        'ApiPlatform\\Serializer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/serializer',
+        ),
+        'ApiPlatform\\OpenApi\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/openapi',
+        ),
+        'ApiPlatform\\Metadata\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/metadata',
+        ),
+        'ApiPlatform\\JsonSchema\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/json-schema',
+        ),
+        'ApiPlatform\\JsonLd\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/jsonld',
+        ),
+        'ApiPlatform\\Hydra\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/hydra',
+        ),
+        'ApiPlatform\\HttpCache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/http-cache',
+        ),
+        'ApiPlatform\\Documentation\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/documentation',
+        ),
+        'ApiPlatform\\Doctrine\\Orm\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/doctrine-orm',
+        ),
+        'ApiPlatform\\Doctrine\\Common\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/api-platform/doctrine-common',
         ),
     );
 
